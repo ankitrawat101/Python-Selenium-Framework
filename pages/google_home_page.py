ca66babc303966895080a9a4efb4a8cd41ee5d02
Abhinay Gkv <abhinaynegiw@gmail.com>
2024-03-05 10:20:48 +0530

    Added page factory method in page file Logs generated

--- pages/google_home_page.py
+++ pages/google_home_page.py
@@ -1,15 +1,24 @@
 from selenium.webdriver.common.by import By
+from seleniumpagefactory import PageFactory
 
 
-class GoogleHomePage:
-    searchField = (By.XPATH, "//textarea[@title='Search']")
-    searchButton = (By.XPATH, "//input[@aria-label='Google Search']")
+class GoogleHomePage(PageFactory):
+    # searchField = (By.XPATH, "//textarea[@title='Search']")
+    # searchButton = (By.XPATH, "//input[@aria-label='Google Search']")
 
     def __init__(self, driver):
+        super().__init__()
         self.driver = driver
 
+    locators = {
+        "searchField": ("XPATH", "//textarea[@title='Search']"),
+        "searchButton": ("XPATH", "//input[@aria-label='Google Search']")
+    }
+
     def enter_search_query(self, query):
-        self.driver.find_element(*GoogleHomePage.searchField).send_keys(query)
+        self.searchField.set_text(query)
+        # self.driver.find_element(*GoogleHomePage.searchField).send_keys(query)
 
     def click_on_search_btn(self):
-        self.driver.find_element(*GoogleHomePage.searchButton).click()
+        self.searchButton.click()
+        # self.driver.find_element(*GoogleHomePage.searchButton).click()
